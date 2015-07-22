54806dfe2c3fd1b11abae0e6ea4e06767b1d6f43
邓景 <dengjing@dengjingdeMacBook-Pro.local>
2015-07-22 16:55:25 +0800

    包分离前的提交

--- client/templates/posts/post_edit.js
+++ client/templates/posts/post_edit.js
@@ -1,6 +1,18 @@
 /**
  * Created by dengjing on 15/7/21.
  */
+Template.postEdit.onCreated(function() {
+    Session.set('postEditErrors', {});
+});
+Template.postEdit.helpers({
+    errorMessage: function(field) {
+        return Session.get('postEditErrors')[field];
+    },
+    errorClass: function (field) {
+        return !!Session.get('postEditErrors')[field] ? 'has-error' : '';
+    }
+});
+
 Template.postEdit.events({
     'submit form': function(e) {
         e.preventDefault();
@@ -12,10 +24,16 @@ Template.postEdit.events({
             title: $(e.target).find('[name=title]').val()
         };
 
+        var errors = validatePost(postProperties);
+        if (errors.title || errors.url) {
+            return Session.set('postEditErrors', errors);
+        }
+
         Posts.update(currentPostId, {$set: postProperties}, function(error) {
             if (error) {
                 // 向用户显示错误信息
-                alert(error.reason);
+                //alert(error.reason);
+                throwError(error.reason);
             } else {
                 Router.go('postPage', {_id: currentPostId});
             }
--- client/templates/posts/post_submit.js
+++ client/templates/posts/post_submit.js
@@ -1,6 +1,18 @@
 /**
  * Created by dengjing on 15/7/20.
  */
+Template.postSubmit.onCreated(function() {
+    Session.set('postSubmitErrors', {});
+});
+Template.postSubmit.helpers({
+    errorMessage: function(field) {
+        return Session.get('postSubmitErrors')[field];
+    },
+    errorClass: function (field) {
+        return !!Session.get('postSubmitErrors')[field] ? 'has-error' : '';
+    }
+});
+
 Template.postSubmit.events({
     'submit form': function(e) {
         e.preventDefault();
@@ -12,14 +24,20 @@ Template.postSubmit.events({
 
         //post._id = Posts.insert(post);
         //Router.go('postPage', post);
+        var errors = validatePost(post);
+        if (errors.title || errors.url) {
+            return Session.set('postSubmitErrors', errors);
+        }
 
         Meteor.call('postInsert', post, function(error, result) {
             // 显示错误信息并退出
             if (error)
-                return alert(error.reason);
+                //return alert(error.reason);
+                return throwError(error.reason);
             // 显示结果，跳转页面
             if (result.postExists)
-                alert('This link has already been posted（该链接已经存在）');
+                //alert('This link has already been posted（该链接已经存在）');
+                throwError('This link has already been posted');
 
             Router.go('postPage', {_id: result._id});
         });
